461ff6b7672ad3efbeb0d5e3ee6810bde92601a6
Luana Leal <luana.leal.eq@gmail.com>
2020-08-01 22:59:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,8 +11,10 @@ end
 #countdown_with_sleep
 
 def countdown_with_sleep(number_secs)
-  
-    
-  
-end
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end 
   
